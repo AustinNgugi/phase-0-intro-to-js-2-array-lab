3e369cdf88b188a184c233534db4277aff60e5ae
Austin <austinngugi000@gmail.com>
2024-03-23 14:24:03 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Write your solution here!
+let cats=
+[
+    "Milo", "Otis" , "Garfield"
+]
+function destructivelyAppendCat(name)
+{
+     cats.push("Ralph");
+     return cats ;
+}
+function destructivelyPrependCat(name)
+{
+     cats.unshift("Bob");
+     return cats ;
+}
+function destructivelyRemoveFirstCat(name)
+{
+     cats.shift("");
+     return cats ;
+}
+function destructivelyRemoveLastCat(name)
+{
+  cats.pop("");
+  return cats ;
+}
+let cats2=[...cats]
+function appendCat(name)
+{
+    cats2.push("Broom")
+    return cats2 ;
+}
+let cats3=[...cats]
+function prependCat(name)
+{
+    cats3.unshift("Arnold")
+    return cats3 ;
+}
+
+let cats4=[...cats]
+{
+    function removeFirstCat(name)
+    {
+        cats4.shift("Milo")
+        return cats4;
+    }
+}
+let cats5=[...cats]
+function removeLastCat()
+{
+    cats5.pop("")
+    return cats5;
+}
